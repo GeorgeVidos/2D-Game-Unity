10facbf4328ef14065a8fb327324fad9b11d94ad
George Vidos <george_vd@hotmail.com>
2022-12-18 20:09:44 +0200

    pathfinding eagle

---
+++ Assets/EnemyGFX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-0.036572f, 0.036572f, 0.036572f);
+        }else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(0.036572f, 0.036572f, 0.036572f);
+        }
+    }
+}
