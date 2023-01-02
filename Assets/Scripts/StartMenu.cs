bf16c1559a76a3173f8a3eca9adfe042f0593cba
George Vidos <george_vd@hotmail.com>
2023-01-02 11:57:45 +0200

    Levels in UI
    Added level selection in the main screen

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -8,4 +8,23 @@ public class StartMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+
+    public void QuitGme()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 }
